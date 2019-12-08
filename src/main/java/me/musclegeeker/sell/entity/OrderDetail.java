4ec6a210d590ec2ddf4166ae64ada39f065a6870
MuscleGeeker <musclegeeker@163.com>
2019-12-08 20:43:14 +0800

    订单逻辑代码提交

--- src/main/java/me/musclegeeker/sell/entity/OrderDetail.java
+++ src/main/java/me/musclegeeker/sell/entity/OrderDetail.java
@@ -3,11 +3,13 @@ package me.musclegeeker.sell.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Data
 @Entity
 public class OrderDetail {
+    @Id
     private String detailId;
     private String orderId;
     private String productId;
